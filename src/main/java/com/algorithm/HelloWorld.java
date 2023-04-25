93e8fec2cc717348f9c9b5a332a8dcb0528cb8de
zyao <zhenjiang.yao@zhangmen.com>
2023-04-25 23:40:15 +0800

    first commit

---
+++ src/main/java/com/algorithm/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.algorithm;
+
+/**
+ * @author zyao
+ * @date 11:05 PM 1/11/2022
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("init");
+    }
+}
